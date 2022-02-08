302f12084cde73f6de1c390fbc93fc19fb66220a
Justin Swift <jlswift2@gmail.com>
2022-02-07 18:16:19 -0600

    commit tree 3f1399080a607b5e195ead55ec09d7f7ca930b98

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((books) => renderBooks(books));
 }
 
 function renderBooks(books) {
@@ -15,3 +16,6 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
